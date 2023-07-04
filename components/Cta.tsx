3309c0b922a8c02841b92343806f11c584896bd9
adzibilal <adzibilal02@gmail.com>
2023-07-04 13:59:51 +0700

    cta

---
+++ components/Cta.tsx
@@ -0,0 +1,27 @@
+import Image from 'next/image'
+import React from 'react'
+
+const Cta = () => {
+    return (
+        <div className='cta w-full mt-10'>
+            <Image
+                src='/cta.jpg'
+                className='w-full'
+                alt=''
+                width={1000}
+                height={50}
+            />
+            <div className='desc-cta flex justify-between items-center py-5'>
+                <div className='title'>
+                    <p className='italic'>#Localbrand</p>
+                    <p className='font-bold text-3xl'>FXXKING COMPASS</p>
+                </div>
+                <div className='btn-cta bg-black text-white py-3 px-10 rounded-full hover:bg-gray-900 cursor-pointer '>
+                    View More
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Cta
